38d3cd29f366157e7f34b40da5cd7537aab3562e
Esmaelhbr <esmael7@live.com>
2021-10-13 16:53:21 -0400

    simple Dom project

---
+++ scripts/showPic.js
@@ -0,0 +1,12 @@
+function showPic(whichpic){
+	let source = whichpic.getAttribute("href");
+	let placeholder= document.getElementById("placeholder")
+	placeholder.setAttribute("src", source);
+
+
+	let text= whichpic.getAttribute("title");
+	let description = document.getElementById("description");
+	description.firstChild.nodeValue = text;
+
+
+}
